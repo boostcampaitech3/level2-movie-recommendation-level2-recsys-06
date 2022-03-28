dc55aae63331eb4b7c4d9e89f9e36f6be3608b00
h-y-e-j-i <11th_hj@naver.com>
2022-03-28 10:06:01 +0000

    multi-VAE

--- run_train.py
+++ run_train.py
@@ -1,14 +1,22 @@
 import argparse
 import os
-
+import time
 import numpy as np
+import pandas as pd
+
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
+import torch.optim as optim
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from importlib import import_module
+from scipy import sparse
+
 
 from datasets import SASRecDataset
-from models import S3RecModel
+from models import S3RecModel, MultiVAE, loss_function_vae
 from trainers import FinetuneTrainer
+import bottleneck as bn
 from utils import (
     EarlyStopping,
     check_path,
@@ -17,10 +25,306 @@ from utils import (
     set_seed,
 )
 
+args = None
+
+class DataLoader():
+    '''
+    Load Movielens dataset
+    '''
+    def __init__(self, path):
+        
+        self.pro_dir = os.path.join(path, 'pro_sg')
+        assert os.path.exists(self.pro_dir), "Preprocessed files do not exist. Run data.py"
+
+        self.n_items = self.load_n_items()
+    
+    def load_data(self, datatype='train'):
+        if datatype == 'train':
+            return self._load_train_data()
+        elif datatype == 'validation':
+            return self._load_tr_te_data(datatype)
+        elif datatype == 'test':
+            return self._load_tr_te_data(datatype)
+        else:
+            raise ValueError("datatype should be in [train, validation, test]")
+        
+    def load_n_items(self):
+        unique_sid = list()
+        with open(os.path.join(self.pro_dir, 'unique_sid.txt'), 'r') as f:
+            for line in f:
+                unique_sid.append(line.strip())
+        n_items = len(unique_sid)
+        return n_items
+    
+    def _load_train_data(self):
+        path = os.path.join(self.pro_dir, 'train.csv')
+        
+        tp = pd.read_csv(path)
+        n_users = tp['uid'].max() + 1
+
+        rows, cols = tp['uid'], tp['sid']
+        data = sparse.csr_matrix((np.ones_like(rows),
+                                 (rows, cols)), dtype='float64',
+                                 shape=(n_users, self.n_items))
+        return data
+    
+    def _load_tr_te_data(self, datatype='test'):
+        tr_path = os.path.join(self.pro_dir, '{}_tr.csv'.format(datatype))
+        te_path = os.path.join(self.pro_dir, '{}_te.csv'.format(datatype))
+
+        tp_tr = pd.read_csv(tr_path)
+        tp_te = pd.read_csv(te_path)
+
+        start_idx = min(tp_tr['uid'].min(), tp_te['uid'].min())
+        end_idx = max(tp_tr['uid'].max(), tp_te['uid'].max())
+
+        rows_tr, cols_tr = tp_tr['uid'] - start_idx, tp_tr['sid']
+        rows_te, cols_te = tp_te['uid'] - start_idx, tp_te['sid']
+
+        data_tr = sparse.csr_matrix((np.ones_like(rows_tr),
+                                    (rows_tr, cols_tr)), dtype='float64', shape=(end_idx - start_idx + 1, self.n_items))
+        data_te = sparse.csr_matrix((np.ones_like(rows_te),
+                                    (rows_te, cols_te)), dtype='float64', shape=(end_idx - start_idx + 1, self.n_items))
+        return data_tr, data_te
+def NDCG_binary_at_k_batch(X_pred, heldout_batch, k=100):
+    '''
+    Normalized Discounted Cumulative Gain@k for binary relevance
+    ASSUMPTIONS: all the 0's in heldout_data indicate 0 relevance
+    '''
+    batch_users = X_pred.shape[0]
+    idx_topk_part = bn.argpartition(-X_pred, k, axis=1)
+    topk_part = X_pred[np.arange(batch_users)[:, np.newaxis],
+                       idx_topk_part[:, :k]]
+    idx_part = np.argsort(-topk_part, axis=1)
+
+    idx_topk = idx_topk_part[np.arange(batch_users)[:, np.newaxis], idx_part]
+
+    tp = 1. / np.log2(np.arange(2, k + 2))
+
+    DCG = (heldout_batch[np.arange(batch_users)[:, np.newaxis],
+                         idx_topk].toarray() * tp).sum(axis=1)
+    IDCG = np.array([(tp[:min(n, k)]).sum()
+                     for n in heldout_batch.getnnz(axis=1)])
+    return DCG / IDCG
+
+
+def Recall_at_k_batch(X_pred, heldout_batch, k=100):
+    batch_users = X_pred.shape[0]
+
+    idx = bn.argpartition(-X_pred, k, axis=1)
+    X_pred_binary = np.zeros_like(X_pred, dtype=bool)
+    X_pred_binary[np.arange(batch_users)[:, np.newaxis], idx[:, :k]] = True
+
+    X_true_binary = (heldout_batch > 0).toarray()
+    tmp = (np.logical_and(X_true_binary, X_pred_binary).sum(axis=1)).astype(
+        np.float32)
+    recall = tmp / np.minimum(k, X_true_binary.sum(axis=1))
+    return recall
+
+def sparse2torch_sparse(data):
+    """
+    Convert scipy sparse matrix to torch sparse tensor with L2 Normalization
+    This is much faster than naive use of torch.FloatTensor(data.toarray())
+    https://discuss.pytorch.org/t/sparse-tensor-use-cases/22047/2
+    """
+    samples = data.shape[0]
+    features = data.shape[1]
+    coo_data = data.tocoo()
+    indices = torch.LongTensor([coo_data.row, coo_data.col])
+    row_norms_inv = 1 / np.sqrt(data.sum(1))
+    row2val = {i : row_norms_inv[i].item() for i in range(samples)}
+    values = np.array([row2val[r] for r in coo_data.row])
+    t = torch.sparse.FloatTensor(indices, torch.from_numpy(values).float(), [samples, features])
+    return t
+
+def naive_sparse2tensor(data):
+    return torch.FloatTensor(data.toarray())
+
+def train(model, criterion, optimizer, is_VAE = False):
+    # Turn on training mode
+    model.train()
+    train_loss = 0.0
+    start_time = time.time()
+    global update_count
+
+    np.random.shuffle(idxlist)
+    
+    for batch_idx, start_idx in enumerate(range(0, N, args.batch_size)):
+        end_idx = min(start_idx + args.batch_size, N)
+        data = train_data[idxlist[start_idx:end_idx]]
+        data = naive_sparse2tensor(data).to(device)
+        optimizer.zero_grad()
+
+        if is_VAE:
+          if args.total_anneal_steps > 0:
+            anneal = min(args.anneal_cap, 
+                            1. * update_count / args.total_anneal_steps)
+          else:
+              anneal = args.anneal_cap
+
+          optimizer.zero_grad()
+          recon_batch, mu, logvar = model(data)
+          
+          loss = criterion(recon_batch, data, mu, logvar, anneal)
+        else:
+          recon_batch = model(data)
+          loss = criterion(recon_batch, data)
+
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+
+        update_count += 1
+
+        if batch_idx % args.log_interval == 0 and batch_idx > 0:
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:4d}/{:4d} batches | ms/batch {:4.2f} | '
+                    'loss {:4.2f}'.format(
+                        epoch, batch_idx, len(range(0, N, args.batch_size)),
+                        elapsed * 1000 / args.log_interval,
+                        train_loss / args.log_interval))
+            
+
+            start_time = time.time()
+            train_loss = 0.0
+
+def evaluate(model, criterion, data_tr, data_te, is_VAE=False):
+    # Turn on evaluation mode
+    model.eval()
+    total_loss = 0.0
+    global update_count
+    e_idxlist = list(range(data_tr.shape[0]))
+    e_N = data_tr.shape[0]
+    n100_list = []
+    r20_list = []
+    r50_list = []
+    
+    with torch.no_grad():
+        for start_idx in range(0, e_N, args.batch_size):
+            end_idx = min(start_idx + args.batch_size, N)
+            data = data_tr[e_idxlist[start_idx:end_idx]]
+            heldout_data = data_te[e_idxlist[start_idx:end_idx]]
+
+            device = torch.device("cuda" if args.cuda else "cpu")
+            data_tensor = naive_sparse2tensor(data).to(device)
+            if is_VAE :
+              
+              if args.total_anneal_steps > 0:
+                  anneal = min(args.anneal_cap, 
+                                1. * update_count / args.total_anneal_steps)
+              else:
+                  anneal = args.anneal_cap
+
+              recon_batch, mu, logvar = model(data_tensor)
+
+              loss = criterion(recon_batch, data_tensor, mu, logvar, anneal)
+
+            else :
+              recon_batch = model(data_tensor)
+              loss = criterion(recon_batch, data_tensor)
+
+
+
+
+            total_loss += loss.item()
+
+            # Exclude examples from training set
+            recon_batch = recon_batch.cpu().numpy()
+            recon_batch[data.nonzero()] = -np.inf
+
+            n100 = NDCG_binary_at_k_batch(recon_batch, heldout_data, 100)
+            r20 = Recall_at_k_batch(recon_batch, heldout_data, 20)
+            r50 = Recall_at_k_batch(recon_batch, heldout_data, 50)
+
+            n100_list.append(n100)
+            r20_list.append(r20)
+            r50_list.append(r50)
+ 
+    total_loss /= len(range(0, e_N, args.batch_size))
+    n100_list = np.concatenate(n100_list)
+    r20_list = np.concatenate(r20_list)
+    r50_list = np.concatenate(r50_list)
+
+    return total_loss, np.mean(n100_list), np.mean(r20_list), np.mean(r50_list)
 
 def main():
+    
+
+    set_seed(args.seed)
+    check_path(args.output_dir)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    args.cuda_condition = torch.cuda.is_available() and not args.no_cuda
+
+    args.data_file = args.data_dir + "train_ratings.csv"
+    item2attribute_file = args.data_dir + args.data_name + "_item2attributes.json"
+
+    user_seq, max_item, valid_rating_matrix, test_rating_matrix, _ = get_user_seqs(
+        args.data_file
+    )
+
+    item2attribute, attribute_size = get_item2attribute_json(item2attribute_file)
+
+    args.item_size = max_item + 2
+    args.mask_id = max_item + 1
+    args.attribute_size = attribute_size + 1
+
+    # save model args
+    args_str = f"{args.model_name}-{args.data_name}"
+    args.log_file = os.path.join(args.output_dir, args_str + ".txt")
+
+    args.item2attribute = item2attribute
+    # set item score in train set to `0` in validation
+    args.train_matrix = valid_rating_matrix
+
+    # save model
+    checkpoint = args_str + ".pt"
+    args.checkpoint_path = os.path.join(args.output_dir, checkpoint)
+
+    
+
+    # train_dataset = SASRecDataset(args, user_seq, data_type="train")
+    # train_sampler = RandomSampler(train_dataset)
+    # train_dataloader = DataLoader(
+    #     train_dataset, sampler=train_sampler, batch_size=args.batch_size
+    # )
+
+    # eval_dataset = SASRecDataset(args, user_seq, data_type="valid")
+    # eval_sampler = SequentialSampler(eval_dataset)
+    # eval_dataloader = DataLoader(
+    #     eval_dataset, sampler=eval_sampler, batch_size=args.batch_size
+    # )
+
+    # test_dataset = SASRecDataset(args, user_seq, data_type="test")
+    # test_sampler = SequentialSampler(test_dataset)
+    # test_dataloader = DataLoader(
+    #     test_dataset, sampler=test_sampler, batch_size=args.batch_size
+    # )
+
+
+    # trainer = FinetuneTrainer(
+    #     model, train_dataloader, eval_dataloader, test_dataloader, None, args
+    # )
+
+    if args.using_pretrain:
+        pretrained_path = os.path.join(args.output_dir, {args.using_pretrain_model_name}+'.pt')
+        try:
+            trainer.load(pretrained_path)
+            print(f"Load Checkpoint From {pretrained_path}!")
+
+        except FileNotFoundError:
+            print(f"{pretrained_path} Not Found! The Model is same as SASRec")
+    else:
+        print("Not using pretrained model. The Model is same as SASRec")
+
+    
+    
+
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="S3RecModel", type=str, help="model type") # model 모듈
+    # parser.add_argument("--model", default="S3RecModel", type=str, help="model type") # model 모듈
     parser.add_argument("--data_dir", default="../data/train/", type=str)
     parser.add_argument("--output_dir", default="output/", type=str)
     parser.add_argument("--data_name", default="Ml", type=str)
@@ -73,94 +377,73 @@ def main():
 
     args = parser.parse_args()
 
-    set_seed(args.seed)
-    check_path(args.output_dir)
+    device = torch.device("cuda" if args.cuda else "cpu")
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
-    args.cuda_condition = torch.cuda.is_available() and not args.no_cuda
+   
+    ###############################################################################
+    # Load data
+    ###############################################################################
 
-    args.data_file = args.data_dir + "train_ratings.csv"
-    item2attribute_file = args.data_dir + args.data_name + "_item2attributes.json"
+    loader = DataLoader(args.data)
 
-    user_seq, max_item, valid_rating_matrix, test_rating_matrix, _ = get_user_seqs(
-        args.data_file
-    )
+    n_items = loader.load_n_items()
+    train_data = loader.load_data('train')
+    vad_data_tr, vad_data_te = loader.load_data('validation')
+    test_data_tr, test_data_te = loader.load_data('test')
 
-    item2attribute, attribute_size = get_item2attribute_json(item2attribute_file)
+    N = train_data.shape[0]
+    idxlist = list(range(N))
 
-    args.item_size = max_item + 2
-    args.mask_id = max_item + 1
-    args.attribute_size = attribute_size + 1
+    ###############################################################################
+    # Build the model
+    ###############################################################################
 
-    # save model args
-    args_str = f"{args.model_name}-{args.data_name}"
-    args.log_file = os.path.join(args.output_dir, args_str + ".txt")
+    p_dims = [200, 600, n_items]
+    model = MultiVAE(p_dims).to(device)
 
-    args.item2attribute = item2attribute
-    # set item score in train set to `0` in validation
-    args.train_matrix = valid_rating_matrix
+    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=args.wd)
+    criterion = loss_function_vae
 
-    # save model
-    checkpoint = args_str + ".pt"
-    args.checkpoint_path = os.path.join(args.output_dir, checkpoint)
+    ###############################################################################
+    # Training code
+    ###############################################################################
 
-    train_dataset = SASRecDataset(args, user_seq, data_type="train")
-    train_sampler = RandomSampler(train_dataset)
-    train_dataloader = DataLoader(
-        train_dataset, sampler=train_sampler, batch_size=args.batch_size
-    )
+    best_n100 = -np.inf
+    update_count = 0
 
-    eval_dataset = SASRecDataset(args, user_seq, data_type="valid")
-    eval_sampler = SequentialSampler(eval_dataset)
-    eval_dataloader = DataLoader(
-        eval_dataset, sampler=eval_sampler, batch_size=args.batch_size
-    )
-
-    test_dataset = SASRecDataset(args, user_seq, data_type="test")
-    test_sampler = SequentialSampler(test_dataset)
-    test_dataloader = DataLoader(
-        test_dataset, sampler=test_sampler, batch_size=args.batch_size
-    )
-
-    # model 모듈
-    #model = S3RecModel(args=args)
-    model_module = getattr(import_module("model"), args.model)
-    model = model_module(args=args)
-    #model = FactorizationMachine(args=args)
-
-    trainer = FinetuneTrainer(
-        model, train_dataloader, eval_dataloader, test_dataloader, None, args
-    )
-
-    if args.using_pretrain:
-        pretrained_path = os.path.join(args.output_dir, {args.using_pretrain_model_name}+'.pt')
-        try:
-            trainer.load(pretrained_path)
-            print(f"Load Checkpoint From {pretrained_path}!")
-
-        except FileNotFoundError:
-            print(f"{pretrained_path} Not Found! The Model is same as SASRec")
-    else:
-        print("Not using pretrained model. The Model is same as SASRec")
+    best_n100 = -np.inf
+    update_count = 0
 
     early_stopping = EarlyStopping(args.checkpoint_path, patience=10, verbose=True)
-    for epoch in range(args.epochs):
-        trainer.train(epoch)
 
-        scores, _ = trainer.valid(epoch)
+    for epoch in range(1, args.epochs + 1):
+        epoch_start_time = time.time()
+        train(model, criterion, optimizer, is_VAE=True)
+        val_loss, n100, r20, r50 = evaluate(model, criterion, vad_data_tr, vad_data_te, is_VAE=True)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:4.2f}s | valid loss {:4.2f} | '
+                'n100 {:5.3f} | r20 {:5.3f} | r50 {:5.3f}'.format(
+                    epoch, time.time() - epoch_start_time, val_loss,
+                    n100, r20, r50))
+        print('-' * 89)
 
-        early_stopping(np.array(scores[-1:]), trainer.model)
-        if early_stopping.early_stop:
-            print("Early stopping")
-            break
+        n_iter = epoch * len(range(0, N, args.batch_size))
 
-    trainer.args.train_matrix = test_rating_matrix
-    print("---------------Change to test_rating_matrix!-------------------")
-    # load the best model
-    trainer.model.load_state_dict(torch.load(args.checkpoint_path))
-    scores, result_info = trainer.test(0)
-    print(result_info)
+        
+    
+        # Save the model if the n100 is the best we've seen so far.
+        if n100 > best_n100:
+            with open(args.save, 'wb') as f:
+                torch.save(model, f)
+            best_n100 = n100
 
+    # Load the best saved model.
+    with open(args.save, 'rb') as f:
+        model = torch.load(f)
 
-if __name__ == "__main__":
-    main()
+    # Run on test data.
+    test_loss, n100, r20, r50 = evaluate(model, criterion, test_data_tr, test_data_te, is_VAE=True)
+    print('=' * 89)
+    print('| End of training | test loss {:4.2f} | n100 {:4.2f} | r20 {:4.2f} | '
+            'r50 {:4.2f}'.format(test_loss, n100, r20, r50))
+    print('=' * 89)
