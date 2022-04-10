72e269e991395faf4794c100d22168664c2c0c91
inhyeokJeon <poiu2389@naver.com>
2022-04-10 23:04:04 +0900

    year 사용했지만 같은 성능 ㅜㅜ

--- models.py
+++ models.py
@@ -1,254 +1,17 @@
-import torch
-import torch.nn as nn
-
-from modules import Encoder, LayerNorm
-
-
-class S3RecModel(nn.Module):
-    def __init__(self, args):
-        super(S3RecModel, self).__init__()
-        self.item_embeddings = nn.Embedding(
-            args.item_size, args.hidden_size, padding_idx=0
-        )
-        self.attribute_embeddings = nn.Embedding(
-            args.attribute_size, args.hidden_size, padding_idx=0
-        )
-        self.position_embeddings = nn.Embedding(args.max_seq_length, args.hidden_size)
-        self.item_encoder = Encoder(args)
-        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
-        self.dropout = nn.Dropout(args.hidden_dropout_prob)
-        self.args = args
-
-        # add unique dense layer for 4 losses respectively
-        self.aap_norm = nn.Linear(args.hidden_size, args.hidden_size)
-        self.mip_norm = nn.Linear(args.hidden_size, args.hidden_size)
-        self.map_norm = nn.Linear(args.hidden_size, args.hidden_size)
-        self.sp_norm = nn.Linear(args.hidden_size, args.hidden_size)
-        self.criterion = nn.BCELoss(reduction="none")
-        self.apply(self.init_weights)
-
-    # AAP
-    def associated_attribute_prediction(self, sequence_output, attribute_embedding):
-        """
-        :param sequence_output: [B L H]
-        :param attribute_embedding: [arribute_num H]
-        :return: scores [B*L tag_num]
-        """
-        sequence_output = self.aap_norm(sequence_output)  # [B L H]
-        sequence_output = sequence_output.view(
-            [-1, self.args.hidden_size, 1]
-        )  # [B*L H 1]
-        # [tag_num H] [B*L H 1] -> [B*L tag_num 1]
-        score = torch.matmul(attribute_embedding, sequence_output)
-        return torch.sigmoid(score.squeeze(-1))  # [B*L tag_num]
-
-    # MIP sample neg items
-    def masked_item_prediction(self, sequence_output, target_item):
-        """
-        :param sequence_output: [B L H]
-        :param target_item: [B L H]
-        :return: scores [B*L]
-        """
-        sequence_output = self.mip_norm(
-            sequence_output.view([-1, self.args.hidden_size])
-        )  # [B*L H]
-        target_item = target_item.view([-1, self.args.hidden_size])  # [B*L H]
-        score = torch.mul(sequence_output, target_item)  # [B*L H]
-        return torch.sigmoid(torch.sum(score, -1))  # [B*L]
-
-    # MAP
-    def masked_attribute_prediction(self, sequence_output, attribute_embedding):
-        sequence_output = self.map_norm(sequence_output)  # [B L H]
-        sequence_output = sequence_output.view(
-            [-1, self.args.hidden_size, 1]
-        )  # [B*L H 1]
-        # [tag_num H] [B*L H 1] -> [B*L tag_num 1]
-        score = torch.matmul(attribute_embedding, sequence_output)
-        return torch.sigmoid(score.squeeze(-1))  # [B*L tag_num]
-
-    # SP sample neg segment
-    def segment_prediction(self, context, segment):
-        """
-        :param context: [B H]
-        :param segment: [B H]
-        :return:
-        """
-        context = self.sp_norm(context)
-        score = torch.mul(context, segment)  # [B H]
-        return torch.sigmoid(torch.sum(score, dim=-1))  # [B]
-
-    #
-    def add_position_embedding(self, sequence):
-
-        seq_length = sequence.size(1)
-        position_ids = torch.arange(
-            seq_length, dtype=torch.long, device=sequence.device
-        )
-        position_ids = position_ids.unsqueeze(0).expand_as(sequence)
-        item_embeddings = self.item_embeddings(sequence)
-        position_embeddings = self.position_embeddings(position_ids)
-        sequence_emb = item_embeddings + position_embeddings
-        sequence_emb = self.LayerNorm(sequence_emb)
-        sequence_emb = self.dropout(sequence_emb)
-
-        return sequence_emb
-
-    def pretrain(
-            self,
-            attributes,
-            masked_item_sequence,
-            pos_items,
-            neg_items,
-            masked_segment_sequence,
-            pos_segment,
-            neg_segment,
-    ):
-
-        # Encode masked sequence
-
-        sequence_emb = self.add_position_embedding(masked_item_sequence)
-        sequence_mask = (masked_item_sequence == 0).float() * -1e8
-        sequence_mask = torch.unsqueeze(torch.unsqueeze(sequence_mask, 1), 1)
-
-        encoded_layers = self.item_encoder(
-            sequence_emb, sequence_mask, output_all_encoded_layers=True
-        )
-        # [B L H]
-        sequence_output = encoded_layers[-1]
-
-        attribute_embeddings = self.attribute_embeddings.weight
-        # AAP
-        aap_score = self.associated_attribute_prediction(
-            sequence_output, attribute_embeddings
-        )
-        aap_loss = self.criterion(
-            aap_score, attributes.view(-1, self.args.attribute_size).float()
-        )
-        # only compute loss at non-masked position
-        aap_mask = (masked_item_sequence != self.args.mask_id).float() * (
-                masked_item_sequence != 0
-        ).float()
-        aap_loss = torch.sum(aap_loss * aap_mask.flatten().unsqueeze(-1))
-
-        # MIP
-        pos_item_embs = self.item_embeddings(pos_items)
-        neg_item_embs = self.item_embeddings(neg_items)
-        pos_score = self.masked_item_prediction(sequence_output, pos_item_embs)
-        neg_score = self.masked_item_prediction(sequence_output, neg_item_embs)
-        mip_distance = torch.sigmoid(pos_score - neg_score)
-        mip_loss = self.criterion(
-            mip_distance, torch.ones_like(mip_distance, dtype=torch.float32)
-        )
-        mip_mask = (masked_item_sequence == self.args.mask_id).float()
-        mip_loss = torch.sum(mip_loss * mip_mask.flatten())
-
-        # MAP
-        map_score = self.masked_attribute_prediction(
-            sequence_output, attribute_embeddings
-        )
-        map_loss = self.criterion(
-            map_score, attributes.view(-1, self.args.attribute_size).float()
-        )
-        map_mask = (masked_item_sequence == self.args.mask_id).float()
-        map_loss = torch.sum(map_loss * map_mask.flatten().unsqueeze(-1))
-
-        # SP
-        # segment context
-        segment_context = self.add_position_embedding(masked_segment_sequence)
-        segment_mask = (masked_segment_sequence == 0).float() * -1e8
-        segment_mask = torch.unsqueeze(torch.unsqueeze(segment_mask, 1), 1)
-        segment_encoded_layers = self.item_encoder(
-            segment_context, segment_mask, output_all_encoded_layers=True
-        )
-
-        # take the last position hidden as the context
-        segment_context = segment_encoded_layers[-1][:, -1, :]  # [B H]
-        # pos_segment
-        pos_segment_emb = self.add_position_embedding(pos_segment)
-        pos_segment_mask = (pos_segment == 0).float() * -1e8
-        pos_segment_mask = torch.unsqueeze(torch.unsqueeze(pos_segment_mask, 1), 1)
-        pos_segment_encoded_layers = self.item_encoder(
-            pos_segment_emb, pos_segment_mask, output_all_encoded_layers=True
-        )
-        pos_segment_emb = pos_segment_encoded_layers[-1][:, -1, :]
-
-        # neg_segment
-        neg_segment_emb = self.add_position_embedding(neg_segment)
-        neg_segment_mask = (neg_segment == 0).float() * -1e8
-        neg_segment_mask = torch.unsqueeze(torch.unsqueeze(neg_segment_mask, 1), 1)
-        neg_segment_encoded_layers = self.item_encoder(
-            neg_segment_emb, neg_segment_mask, output_all_encoded_layers=True
-        )
-        neg_segment_emb = neg_segment_encoded_layers[-1][:, -1, :]  # [B H]
-
-        pos_segment_score = self.segment_prediction(segment_context, pos_segment_emb)
-        neg_segment_score = self.segment_prediction(segment_context, neg_segment_emb)
-
-        sp_distance = torch.sigmoid(pos_segment_score - neg_segment_score)
-
-        sp_loss = torch.sum(
-            self.criterion(
-                sp_distance, torch.ones_like(sp_distance, dtype=torch.float32)
-            )
-        )
-
-        return aap_loss, mip_loss, map_loss, sp_loss
-
-    # Fine tune
-    # same as SASRec
-    def finetune(self, input_ids):
-
-        attention_mask = (input_ids > 0).long()
-        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(
-            2
-        )  # torch.int64
-        max_len = attention_mask.size(-1)
-        attn_shape = (1, max_len, max_len)
-        subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1)  # torch.uint8
-        subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
-        subsequent_mask = subsequent_mask.long()
-
-        if self.args.cuda_condition:
-            subsequent_mask = subsequent_mask.cuda()
-
-        extended_attention_mask = extended_attention_mask * subsequent_mask
-        extended_attention_mask = extended_attention_mask.to(
-            dtype=next(self.parameters()).dtype
-        )  # fp16 compatibility
-        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
-
-        sequence_emb = self.add_position_embedding(input_ids)
-
-        item_encoded_layers = self.item_encoder(
-            sequence_emb, extended_attention_mask, output_all_encoded_layers=True
-        )
-
-        sequence_output = item_encoded_layers[-1]
-        return sequence_output  # B x L x H
-
-    def init_weights(self, module):
-        """Initialize the weights."""
-        if isinstance(module, (nn.Linear, nn.Embedding)):
-            # Slightly different from the TF version which uses truncated_normal for initialization
-            # cf https://github.com/pytorch/pytorch/pull/5617
-            module.weight.data.normal_(mean=0.0, std=self.args.initializer_range)
-        elif isinstance(module, LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-        if isinstance(module, nn.Linear) and module.bias is not None:
-            module.bias.data.zero_()
-
-
-from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
+
+from scipy.sparse import csr_matrix
 from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
+import wandb
 
 class EASE:
     def __init__(self):
         self.user_enc = LabelEncoder()
         self.item_enc = LabelEncoder()
+        self.last_interaction = LabelEncoder()
+        self.item_year = LabelEncoder()
 
     def _get_users_and_items(self, df):
         users = self.user_enc.fit_transform(df.loc[:, 'user'])
@@ -266,7 +29,7 @@ class EASE:
 
         X = csr_matrix((values, (users, items)))
         self.X = X
-
+        print("finish X matrix")
         G = X.T.dot(X).toarray()
         diagIndices = np.diag_indices(G.shape[0])
         G[diagIndices] += lambda_
@@ -276,6 +39,7 @@ class EASE:
 
         self.B = B
         self.pred = X.dot(B)
+        print("finish B matrix")
 
     def predict(self, train, users, items, k):
         df = pd.DataFrame()
@@ -283,18 +47,121 @@ class EASE:
         dd = train.loc[train.user.isin(users)]
         dd['ci'] = self.item_enc.transform(dd.item)
         dd['cu'] = self.user_enc.transform(dd.user)
+        dd['last_interaction'] = dd.last_interaction
+        dd['item_year'] = dd.item_year
+
         g = dd.groupby('user')
         for user, group in tqdm(g):
-            watched = set(group['ci'])
+            watched = set(group['ci']) # 본 영화
+
+            # 본 영화를 item 중에 제거한 index list.
             candidates = [item for item in items if item not in watched]
-            u = group['cu'].iloc[0]
-            pred = np.take(self.pred[u, :], candidates)
-            res = np.argpartition(pred, -k)[-k:]
+            last_interaction = group['last_interaction'].iloc[group['cu'].iloc[0]]
+            item_year = group['item_year']
+            u = group['cu'].iloc[0] # 예측한 user 번호
+            pred = np.take(self.pred[u, :], candidates) # 유저의 예측 값중 후보들만 가져온다.
+            res = np.argpartition(pred, -k)[-k:] # Top 10
             r = pd.DataFrame({
                 "user": [user] * len(res),
                 "item": np.take(candidates, res),
+                #"score": np.take(pred, res)
+            }) #.sort_values('score', ascending=False)
+            df = df.append(r, ignore_index=True)
+        df['item'] = self.item_enc.inverse_transform(df['item'])
+        print("finish predict")
+        return df
+
+class EASE2:
+    def __init__(self, train):
+        self.user_enc = LabelEncoder()
+        self.item_enc = LabelEncoder()
+        self.last_interaction = LabelEncoder()
+        self.item_year = LabelEncoder()
+
+        self.train = train
+
+        self.item_year_df = train[['item', 'item_year']]
+        self.user_last_interaction = train[['user', 'last_interaction']]
+
+        item_year_list = [1920, 1921, 1916, 1917, 1902, 1915, 2015, 1919]
+        for index, item in enumerate([6987, 3310, 7243, 8511, 32898, 7065, 119145, 6988]):
+            self.item_year_df.loc[self.item_year_df.item == item, ('item_year')] = (item_year_list[index])
+
+        self.item_year_df.drop_duplicates(['item','item_year'])
+        self.user_last_interaction.drop_duplicates(['user','last_interaction'])
+        self.item_year_dict = self.item_year_df.set_index('item').to_dict()['item_year']
+        self.user_last_interaction_dict = self.user_last_interaction.set_index('user').to_dict()['last_interaction']
+
+        print("init finished")
+
+    def _get_users_and_items(self, df):
+        users = self.user_enc.fit_transform(df.loc[:, 'user'])
+        items = self.item_enc.fit_transform(df.loc[:, 'item'])
+        return users, items
+
+    def fit(self, df, lambda_: float = 0.5, implicit=True):
+        """
+        df: pandas.DataFrame with columns user_id, item_id and (rating)
+        lambda_: l2-regularization term
+        implicit: if True, ratings are ignored and taken as 1, else normalized ratings are used
+        """
+        users, items = self._get_users_and_items(df)
+        values = np.ones(df.shape[0]) if implicit else df['rating'].to_numpy() / df['rating'].max()
+
+        X = csr_matrix((values, (users, items)))
+        self.X = X
+        print("finish X matrix")
+        G = X.T.dot(X).toarray()
+        diagIndices = np.diag_indices(G.shape[0])
+        G[diagIndices] += lambda_
+        P = np.linalg.inv(G)
+        B = P / (-np.diag(P))
+        B[diagIndices] = 0
+
+        self.B = B
+        self.pred = X.dot(B)
+        print("finish B matrix")
+
+    def predict(self, train, users, items, k):
+        df = pd.DataFrame()
+        items = self.item_enc.transform(items)
+        dd = train.loc[train.user.isin(users)]
+        dd['ci'] = self.item_enc.transform(dd.item)
+        dd['cu'] = self.user_enc.transform(dd.user)
+
+        g = dd.groupby('user')
+        for user, group in tqdm(g):
+            # wandb.log({'user': user})
+            watched = set(group['ci']) # 본 영화
+
+            # 본 영화를 item 중에 제거한 index list.
+            candidates = [item for item in items if item not in watched]
+            last_interaction = self.user_last_interaction_dict[user]
+            # last_interaction = \
+            # self.user_last_interaction[self.user_last_interaction['user'] == user]['last_interaction'].iloc[0]
+            will_be_removed = []
+
+            original_cand = self.item_enc.inverse_transform(candidates)
+
+            for index, movie_number in enumerate(original_cand):
+                movie_year = self.item_year_dict[movie_number]
+                # movie_year = self.item_year_df[self.item_year_df['item'] == movie_number]['item_year'].iloc[0]
+                if int(movie_year) > last_interaction:
+                    will_be_removed.append(index)
+
+            final_cand = np.delete(candidates, will_be_removed)
+
+            u = group['cu'].iloc[0] # 예측한 user 번호
+            pred = np.take(self.pred[u, :], final_cand) # 유저의 예측 값중 후보들만 가져온다.
+            res = np.argpartition(pred, -k)[-k:] # Top 10
+            r = pd.DataFrame({
+                "user": [user] * len(res),
+                "item": np.take(final_cand, res),
                 "score": np.take(pred, res)
             }).sort_values('score', ascending=False)
             df = df.append(r, ignore_index=True)
+
+
         df['item'] = self.item_enc.inverse_transform(df['item'])
+        print("finish predict")
         return df
